70422a6e345dcdac4d46e479f785fe5b0d8139d8
Tran Bao Viet <85724720+baoviettran@users.noreply.github.com>
2021-11-27 23:36:02 +0700

    Add "name" fields and more details in Sound() methods

--- BaitapTuan1/Bai5/Program.cs
+++ BaitapTuan1/Bai5/Program.cs
@@ -10,26 +10,38 @@ namespace Bai5
     {
         static void Main(string[] args)
         {
-            Cat khoaiLangTheCat = new Cat();
+            Cat khoaiLangTheCat = new Cat("Khoai Lang");
             khoaiLangTheCat.Sound();
-            Dog jonTheDog = new Dog();
+            Dog jonTheDog = new Dog("Jon");
             jonTheDog.Sound();
             Console.ReadLine();
         }
     }
     class Cat : IAnimal
     {
+        string name;
+
+        public Cat(string name)
+        {
+            this.name = name;
+        }
         public void Sound()
         {
-            Console.WriteLine("Meow meow!");
+            Console.WriteLine($"The cat {this.name} says \"Meow meow!\".");
         }
     }
 
     class Dog : IAnimal
     {
+        string name;
+
+        public Dog(string name)
+        {
+            this.name = name;
+        }
         public void Sound()
         {
-            Console.WriteLine("Go go!");
+            Console.WriteLine($"The dog {this.name} says \"Go go!\".");
         }
 
     }
