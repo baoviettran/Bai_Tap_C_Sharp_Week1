e02fe0f2266880fdb585eb339ca328168ce801de
Tran Bao Viet <85724720+baoviettran@users.noreply.github.com>
2021-11-28 00:33:42 +0700

    Task 2 v1

---
+++ BaitapTuan1/Bai2_HashTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Bai2_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add("1", "one");
+            hash.Add("2", "two");
+            hash.Add("3", "three");
+            //foreach(DictionaryEntry item in hash)
+            //{
+            //    Console.WriteLine($"{item.Key}\t{item.Value}");
+            //}
+            Console.WriteLine("Hashtable has already been initialized.\nPlease enter the KEY for searching:");
+            object inputKey = Console.ReadLine();
+            string strKey = Convert.ToString(inputKey);
+            if (hash.ContainsKey(inputKey))
+            {
+                Console.WriteLine("KEY:\tVALUE:");
+                Console.WriteLine();
+                Console.WriteLine($"{strKey}\t{hash[strKey]}");
+            }
+            else
+            {
+                Console.WriteLine($"The key \"{strKey}\" does not exist in the hashtable.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
