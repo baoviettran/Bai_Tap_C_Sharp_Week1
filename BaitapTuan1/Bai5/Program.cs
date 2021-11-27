3ce51b865b68f52650186e8199ec77cbe5b4a455
Tran Bao Viet <85724720+baoviettran@users.noreply.github.com>
2021-11-27 23:25:29 +0700

    Task 5 commit

---
+++ BaitapTuan1/Bai5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat khoaiLangTheCat = new Cat();
+            khoaiLangTheCat.Sound();
+            Dog jonTheDog = new Dog();
+            jonTheDog.Sound();
+            Console.ReadLine();
+        }
+    }
+    class Cat : IAnimal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Meow meow!");
+        }
+    }
+
+    class Dog : IAnimal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Go go!");
+        }
+
+    }
+}
