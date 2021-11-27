c408a7327be83dbaa7836d2155a4c66f46c6d079
Tran Bao Viet <85724720+baoviettran@users.noreply.github.com>
2021-11-27 23:20:24 +0700

    Task 4 commit

---
+++ BaitapTuan1/Bai4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat khoaiLangTheCat = new Cat();
+            khoaiLangTheCat.Sound();
+            Dog jonTheDog = new Dog();
+            jonTheDog.Sound();
+            Console.ReadLine();
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Meow meow!");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Go go!");
+        }
+
+    }
+}
