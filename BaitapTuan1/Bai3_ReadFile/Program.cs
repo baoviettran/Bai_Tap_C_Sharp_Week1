2542f6c2ba34682d5750d1bb36975524d97c6aaa
Tran Bao Viet <85724720+baoviettran@users.noreply.github.com>
2021-11-28 17:31:34 +0700

    Task3 v1

---
+++ BaitapTuan1/Bai3_ReadFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Bai3_ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList studentList = new ArrayList();
+            try
+            {
+                StreamReader sr = new StreamReader("students.txt");
+                string line;
+                int count = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    object studentInfo = line;
+                    studentList.Add(studentInfo);
+                    count++;
+                    Console.WriteLine($"{count} objects has been added into the list.");
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
