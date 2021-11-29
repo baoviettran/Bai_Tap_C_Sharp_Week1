5583b666cbab729caefd9ff50dfc8c9c24978ba5
Hồ Trí Dũng <37924034+hotridung@users.noreply.github.com>
2021-11-29 08:39:03 +0700

    Review 29/11/2021

--- BaitapTuan1/Bai1/Program.cs
+++ BaitapTuan1/Bai1/Program.cs
@@ -20,7 +20,7 @@ namespace Bai1
             Console.WriteLine("Cac so nguyen to co trong mang:");
             for (int j=0;j<1000;++j)
             {
-                if (KtraNto(intArray[j]))
+                if (KtraNto(intArray[j]))//Khong nen goi ham trong cau lenh dieu kien, gan ket qua cho bien va kiem tra gia tri bien
                 {
                     Console.WriteLine(intArray[j]);
                 }
@@ -29,14 +29,14 @@ namespace Bai1
         }
 
         //Ham kiem tra so nguyen to
-        static bool KtraNto(int n)
+        static bool KtraNto(int n)//Khong nen viet tat ten ham. Vi du: kiemtraNguyento(int n)
         {
             bool result = true;
             if (n <= 1)
             {
                 result = false;
             }
-            for (int i = 2; i <= Math.Sqrt(n); ++i)
+            for (int i = 2; i <= Math.Sqrt(n); ++i)//Tai lai lai la Sqrt nhi?
             {
                 if (n % i == 0)
                 {
